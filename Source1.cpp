78ea01e72640df338231c665578aea253d86c14f
Priyanka <priyankadalai902@gmail.com>
2026-06-26 20:53:27 +0530

    Implemented largest of three numbers

--- Source1.cpp
+++ Source1.cpp
@@ -2,6 +2,24 @@
 
 int main()
 {
-	std::cout << "Hello World!";
+	int x, y, z;
+
+	std::cout << "Enter three number:";
+	std::cin >> x >> y >> z;
+
+	int largest = x;
+
+	if (y > largest)
+	{
+		largest = y;
+	}
+
+	if (z > largest)
+	{
+		largest = z;
+	}
+
+	std::cout << largest;
 	return 0;
+
 }
